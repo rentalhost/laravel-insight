c66a2b360abcfcbcef1a6c98a4c011d92d4022d8
David Rodrigues <david.proweb@gmail.com>
2017-05-29 15:42:43 -0300

    PhpExpressionUtil: from() now supports class constants;

--- resources-tests/api/PhpExpressionUtil.warpingLiterals.php
+++ resources-tests/api/PhpExpressionUtil.warpingLiterals.php
@@ -12,3 +12,16 @@ $assignedLiteral = $withSubAssignment = $thirdLevel = WARPING_LITERAL;
 
 $ccUnresolvedConstantReference = CC_UNRESOLVED_REFERENCE;
 
+class ResolvingInsideClass
+{
+    const DIRECT_LITERAL = 'indirectClassValue';
+    const INDIRECT_LITERAL = self::DIRECT_LITERAL;
+    const WARPING_LITERAL = self::INDIRECT_LITERAL;
+
+    public function __construct()
+    {
+        $indirectLiteral = self::INDIRECT_LITERAL;
+        $warpingLiteral = self::WARPING_LITERAL;
+    }
+}
+
--- src/api/PhpExpressionUtil.java
+++ src/api/PhpExpressionUtil.java
@@ -3,6 +3,7 @@ package net.rentalhost.idea.api;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.jetbrains.php.lang.psi.elements.*;
+import com.jetbrains.php.lang.psi.elements.impl.ClassConstImpl;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -12,17 +13,26 @@ public enum PhpExpressionUtil {
 
     @Nullable
     public static PhpExpression from(@NotNull final PhpExpression element) {
-        if (element instanceof ConstantReference) {
-            final Constant constantResolved = (Constant) ((PsiReference) element).resolve();
+        if (element instanceof PhpReference) {
+            final PsiElement constantResolved = ((PsiReference) element).resolve();
 
             if (constantResolved == null) {
                 return null;
             }
 
-            final PsiElement constantResolvedValue = constantResolved.getValue();
-            assert constantResolvedValue != null;
+            if (constantResolved instanceof ClassConstImpl) {
+                final PsiElement constantResolvedValue = ((Field) constantResolved).getDefaultValue();
+                assert constantResolvedValue != null;
 
-            return from((PhpExpression) constantResolvedValue);
+                return from((PhpExpression) constantResolvedValue);
+            }
+
+            if (constantResolved instanceof Constant) {
+                final PsiElement constantResolvedValue = ((Constant) constantResolved).getValue();
+                assert constantResolvedValue != null;
+
+                return from((PhpExpression) constantResolvedValue);
+            }
         }
 
         if (element instanceof AssignmentExpression) {
