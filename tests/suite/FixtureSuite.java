65a6a834a770546c5b1617c441896b1d967371b2
David Rodrigues <david.proweb@gmail.com>
2017-05-30 08:57:32 -0300

    FixtureSuite: optimizing test file parsing;

--- tests/suite/FixtureSuite.java
+++ tests/suite/FixtureSuite.java
@@ -1,11 +1,8 @@
 package net.rentalhost.suite;
 
 import com.intellij.codeInspection.LocalInspectionTool;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
 
 import java.nio.file.Paths;
@@ -31,12 +28,7 @@ public class FixtureSuite extends CodeInsightFixtureTestCase {
 
     @Nullable
     protected PsiFile getResourceFile(final String path) {
-        final String      pathAbsolute = Paths.get("resources-tests/" + path).toAbsolutePath().toString();
-        final VirtualFile virtualFile  = LocalFileSystem.getInstance().findFileByPath(pathAbsolute);
-
-        assert virtualFile != null;
-
-        return PsiManager.getInstance(myFixture.getProject()).findFile(virtualFile);
+        return myFixture.configureByFile("resources-tests/" + path);
     }
 
     @Override
