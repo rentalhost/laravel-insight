2cbca11e740da07503acfdd6390a9ff599843d90
David Rodrigues <david.proweb@gmail.com>
2017-05-30 10:35:37 -0300

    PhpFunctionUtil: implementation (for now, supports only @return annotation);

---
+++ resources-tests/api/PhpFunctionUtil.returnTypes.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @return string */
+function respectPhpdocReturnType_StringOnly()
+{
+}
+
+/** @return string|null */
+function respectPhpdocReturnType_StringOrNull()
+{
+}
+
+/** @return null|string */
+function respectPhpdocReturnType_NullOrString()
+{
+}
+
+/** @return string|int|boolean|float */
+function respectPhpdocReturnType_AllScalarTypes()
+{
+}
+
+/** @return UnresolvableQualifier */
+function respectPhpdocReturnType_UnresolvableQualifier()
+{
+}
