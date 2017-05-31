174cfa40490e439c4153c4804eabe1257ec0a3aa
David Rodrigues <david.proweb@gmail.com>
2017-05-31 16:48:48 -0300

    PhpDocCommentUtil: implemented createTag() that allows create phpdoc tag easily;

---
+++ resources-tests/api/PhpDocCommentUtil.tagsCreation.php
@@ -0,0 +1,33 @@
+<?php
+
+class ClassReference_WithoutAnyPhpDoc
+{
+}
+
+/**
+ * @propertyA
+ * @propertyB
+ */
+class ClassReference_WithPhpDoc
+{
+}
+
+/**
+ */
+class ClassReference_WithEmptyPhpDoc
+{
+}
+
+/** @property */
+class ClassReference_WithInlinePhpDoc
+{
+}
+
+/**
+ * Realworld example.
+ * @param int $id
+ * @return stdClass
+ */
+class ClassReference_WithRealworldPhpdoc
+{
+}
