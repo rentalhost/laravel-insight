60130730a45973ac81c5ee1b38fcfa9e33ed651c
David Rodrigues <david.proweb@gmail.com>
2017-05-31 18:23:47 -0300

    UserDataHolderUtil: implementation;

---
+++ tests/idea/api/UserDataHolderUtilTest.java
@@ -0,0 +1,27 @@
+package net.rentalhost.idea.api;
+
+import com.intellij.openapi.util.Key;
+import com.intellij.psi.PsiFile;
+import org.junit.Assert;
+
+import java.util.List;
+
+import net.rentalhost.suite.FixtureSuite;
+
+public class UserDataHolderUtilTest extends FixtureSuite {
+    public void testFrom() {
+        final PsiFile           sampleFile = getResourceFile("api/DummyFile.php");
+        final Key<List<String>> sampleKey  = new Key("SampleKey");
+
+        final Object instanceOriginal    = UserDataHolderUtil.from(sampleFile, sampleKey, Object::new);
+        final Object instanceShoudBeSame = UserDataHolderUtil.from(sampleFile, sampleKey, Object::new);
+
+        Assert.assertSame(instanceShoudBeSame, instanceOriginal);
+
+        sampleFile.putUserData(sampleKey, null);
+
+        final Object instanceShoudBeAnotherInstance = UserDataHolderUtil.from(sampleFile, sampleKey, Object::new);
+
+        Assert.assertNotSame(instanceShoudBeAnotherInstance, instanceOriginal);
+    }
+}
