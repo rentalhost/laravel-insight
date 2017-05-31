a4b800e565942bea1ab4336cd9234d72af9c799b
David Rodrigues <david.proweb@gmail.com>
2017-05-31 15:57:02 -0300

    FixtureSuite: dropping dead code;

--- tests/suite/FixtureSuite.java
+++ tests/suite/FixtureSuite.java
@@ -1,23 +1,21 @@
 package net.rentalhost.suite;
 
 import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
 import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
 
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class FixtureSuite extends CodeInsightFixtureTestCase {
-    private static String getResourcesPath() {
-        return Paths.get("resources-tests").toAbsolutePath().toString();
-    }
-
     @NotNull
     protected static <T> T valueOf(@Nullable final T element) {
         assert element != null;
