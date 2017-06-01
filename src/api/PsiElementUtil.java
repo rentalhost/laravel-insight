81ff84738613be7e5fd99e5e54d29034a6e18ed8
David Rodrigues <david.proweb@gmail.com>
2017-06-01 09:55:56 -0300

    PsiElementUtil: using unparenthesize() api instead of own code;

--- src/api/PsiElementUtil.java
+++ src/api/PsiElementUtil.java
@@ -2,7 +2,6 @@ package net.rentalhost.idea.api;
 
 import com.intellij.psi.PsiElement;
 import com.jetbrains.php.lang.psi.elements.ParenthesizedExpression;
-import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -11,18 +10,10 @@ public enum PsiElementUtil {
 
     @Nullable
     public static <T extends PsiElement> T skipParentheses(final T elementInitial) {
-        return RecursionResolver.resolve(elementInitial, resolver -> {
-            final PsiElement element = (PsiElement) resolver.getObject();
+        if (elementInitial instanceof ParenthesizedExpression) {
+            return (T) ((ParenthesizedExpression) elementInitial).unparenthesize();
+        }
 
-            if (element instanceof ParenthesizedExpression) {
-                final PhpPsiElement elementArgument = ((ParenthesizedExpression) element).getArgument();
-
-                if (elementArgument != null) {
-                    return (T) resolver.resolve(elementArgument);
-                }
-            }
-
-            return (T) element;
-        });
+        return elementInitial;
     }
 }
